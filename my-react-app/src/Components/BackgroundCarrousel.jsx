cf57494ea493d6b21b533b7956c3d381e09c07fb
Emma-Christie Kameleu <emmakameleu@gmail.com>
2025-06-10 12:08:28 -0400

    Update BackgroundCarrousel.jsx

--- my-react-app/src/Components/BackgroundCarrousel.jsx
+++ my-react-app/src/Components/BackgroundCarrousel.jsx
@@ -15,7 +15,7 @@ function BackgroundCarousel() {
         <Carousel.Item>
           <img
             className="d-block w-100 carousel-image"
-            src="/images/slide2.jpg"
+            src={`${process.env.PUBLIC_URL}/images/slide2.jpg`}
             alt="Slide 2"
           />
         </Carousel.Item>
